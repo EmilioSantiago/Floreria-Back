4f42e7d1709bf189bebf7c518b19ef5aa61d9916
Emilio Santiago <emiliosantiawo@gmail.com>
2026-05-21 16:27:07 -0600

    fix de mercado pago2

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+
+app.use(cors({
+  origin: [
+    'http://localhost:8000',
+    'https://floreria-back-production-0c99.up.railway.app'
+  ],
+  credentials: true
+}));
 const morgan = require('morgan');
 const helmet = require('helmet');
 const path = require('path');
